6f4931084a1031b6a58e16f6df0d072cdd4de283
korneelverbeke <korneel.verbeke1@gmail.com>
2020-08-11 13:11:38 +0200

    add project files

---
+++ Nova/Assets/Scripts/DashToMouse.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashToMouse : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    private void Start()
+    {
+
+    }
+
+
+    void Update ()
+    {
+
+        var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        var mouseDir = mousePos - gameObject.transform.position;
+        mouseDir.z = 0.0f;
+        mouseDir = mouseDir.normalized;
+ 
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(mouseDir * 1500);
+
+            //animator.SetBool("IsDashing", true);
+        }
+
+    }
+
+
+    /*public bool isGrounded()
+    {
+
+    }*/
+
+
+}
