a0ba123a6c53a2db3d70e9601ea2fbd182ff0e6e
RomanVannieuwenhuyse <69513201+RomanVannieuwenhuyse@users.noreply.github.com>
2020-08-11 16:17:21 +0200

    No more jumoing animation in mid-air

--- Nova/Assets/Scripts/CharacterController2D.cs
+++ Nova/Assets/Scripts/CharacterController2D.cs
@@ -13,6 +13,8 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
 
+	Animator animator;
+
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
@@ -129,6 +131,7 @@ public class CharacterController2D : MonoBehaviour
 		{
 			// Add a vertical force to the player.
 			m_Grounded = false;
+			animator.SetBool("IsJumping", true);
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
 		}
 	}
