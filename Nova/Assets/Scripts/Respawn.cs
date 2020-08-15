b07fe2c206e3ca8b9a8a4685309d5747fb208d93
RomanVannieuwenhuyse <69513201+RomanVannieuwenhuyse@users.noreply.github.com>
2020-08-15 09:02:00 +0200

    Respawn script verbeterd
    Ook de ceiling weggehaald, omdat je daar tegen stoot

--- Nova/Assets/Scripts/Respawn.cs
+++ Nova/Assets/Scripts/Respawn.cs
@@ -6,26 +6,44 @@ public class Respawn : MonoBehaviour
 {
 
     public Transform camera;
-    
+
+    public List<Vector2> cameraPositions = new List<Vector2>();
+
+    public List<Vector2> respawnPositions = new List<Vector2>();
+
+    int level;
+
 
     void Start()
     {
-        
+        cameraPositions.Add(new Vector2(0f, 0f));
+
+        cameraPositions.Add(new Vector2(46f, 6f));
+
+        respawnPositions.Add(new Vector2(-22.43298f, -10.112f));
+
+        respawnPositions.Add(new Vector2(23.58f, 4.884028f));
     }
 
-    
+
     void Update()
     {
 
         if (transform.position.y < -14)
         {
-            transform.position = new Vector2(-22.43298f, -10.112f);
-        }  
+            transform.position = respawnPositions[level];
+        }
 
-        if (transform.position.x > 23.5)
+        if (transform.position.x < 23.5)
         {
-            camera.position = new Vector2(46f, 6f);
-        }  
-        
+            camera.position = cameraPositions[0];
+            level = 0;
+        }
+        else
+        {
+            camera.position = cameraPositions[1];
+            level = 1;
+        }
+
     }
 }
