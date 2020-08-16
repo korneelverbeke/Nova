8220f1ace2347ebcc3cfe66f4e0e64ef4650bfff
RomanVannieuwenhuyse <69513201+RomanVannieuwenhuyse@users.noreply.github.com>
2020-08-16 15:32:52 +0200

    Fall

---
+++ Nova/Assets/Scripts/Fall.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    bool dropped = false;
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.tag == "Player" && dropped == false)
+        {
+            StartCoroutine(ShakeAndFall(.15f, .25f));
+
+            dropped = true;
+        }
+    }
+
+    public IEnumerator ShakeAndFall (float duration, float magnitude)
+    {
+
+        Vector2 originalPos = transform.position;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector2(transform.position.x + x, transform.position.y);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = originalPos;
+
+        rb.isKinematic = false;
+
+    }
+}
