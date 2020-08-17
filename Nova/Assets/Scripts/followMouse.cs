eccc383da7d53bc911767469ed50b939b90c9405
RomanVannieuwenhuyse <69513201+RomanVannieuwenhuyse@users.noreply.github.com>
2020-08-17 10:39:25 +0200

    Cursor : )

---
+++ Nova/Assets/Scripts/followMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = new Vector2(Mousepos.x, Mousepos.y);
+    }
+}
