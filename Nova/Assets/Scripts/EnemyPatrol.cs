1c7069b0e9e545d88dd139b3340d35cc572f45f7
korneelverbeke <korneel.verbeke1@gmail.com>
2020-08-18 23:05:22 +0200

    enemy patrol on patrol tilemap + some sprite settings

---
+++ Nova/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+
+    public Transform groundDetection;
+
+    public float  speed;
+
+    private bool movingRight = true;
+
+
+    void Update ()
+    {
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f);
+
+        if(groundInfo.collider != null)
+        {
+            if(groundInfo.collider.tag != "patrol")
+            {
+                if(movingRight == false)
+                {
+                    transform.eulerAngles =  new Vector3(0, -180, 0);
+                    movingRight = true;
+                }
+
+                else 
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = false;
+                }
+            }
+        }
+
+
+
+    }
+
+}
