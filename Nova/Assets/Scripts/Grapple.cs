ac0a6ea734218c38cbf43b5a9f2dd49d0a720eec
RomanVannieuwenhuyse <69513201+RomanVannieuwenhuyse@users.noreply.github.com>
2020-08-18 13:30:59 +0200

    Grappling hook start

--- Nova/Assets/Scripts/Grapple.cs
+++ Nova/Assets/Scripts/Grapple.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Grapple : MonoBehaviour
 {
     Vector2 GrapplePoint;
+
     public LayerMask WhatIsGrappeable;
+
+    public Transform GrapplingRope, player;
+
+    public int maxDistance = 1000;
+
+    SpringJoint2D joint;
 
     void Update()
     {
@@ -17,6 +24,46 @@ public class Grapple : MonoBehaviour
         {
             StopGrappling();
         }
+    }
+
+    void StartGrappling()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+        var mouseDif = mousePos - gameObject.transform.position;
+
+        mouseDif.z = 0.0f;
+
+        float distance = mouseDif.magnitude;
+
+        var mouseDir = mouseDif / distance;
+
+        mouseDir.Normalize();
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+
+        if (hit.collider != null)
+        {
+            GrapplePoint = hit.point;
+
+            joint = player.gameObject.AddComponent<SpringJoint2D>();
+
+            joint.autoConfigureConnectedAnchor = false;
+
+            joint.connectedAnchor = GrapplePoint;
+
+            float distanceFromPoint = Vector2.Distance(player.position, GrapplePoint);
+
+            joint.distance = distanceFromPoint;
+
+            joint.dampingRatio = 7f;
+        }
+
+    }
+
+    void StopGrappling()
+    {
+
     }
 
 }
