7ccc9eaa0a1c1d06b317ec7960154b48e7bb5222
RomanVannieuwenhuyse <69513201+RomanVannieuwenhuyse@users.noreply.github.com>
2020-08-18 12:37:15 +0200

    GRAPPLING

---
+++ Nova/Assets/Scripts/Grapple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    Vector2 GrapplePoint;
+    public LayerMask WhatIsGrappeable;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrappling();
+        }
+            else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrappling();
+        }
+    }
+
+}
